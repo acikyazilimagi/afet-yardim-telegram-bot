a766706022ad956703e959d08b487c4010a7dda5
Can <32599057+CanCodes@users.noreply.github.com>
2023-02-11 15:35:59 +0300

    feat: refactoring for the new Go API

--- main.go
+++ main.go
@@ -11,50 +11,64 @@ import (
 )
 
 const (
-	backendURL = "https://api.afetharita.com/feeds/entries"
+	backendURL = "https://apigo.afetharita.com/events"
 )
 
 type FeedEntry struct {
-	IsResolved bool   `json:"is_resolved"`
-	FullText   string `json:"full_text"`
-	Channel    string `json:"channel"`
+	ID              string `json:"id"`
+	RawText         string `json:"raw_text"`
+	Channel         string `json:"channel"`
+	ExtraParameters string `json:"extra_parameters"`
+	Epoch           int64  `json:"timestamp"`
 }
 
-func newFeedEntry(fullText string) *FeedEntry {
+func newFeedEntry(message *tgbotapi.Message) *FeedEntry {
+	// TODO: maybe we can send username in extra parameters like: {"username": "someUsername"}
 	return &FeedEntry{
-		IsResolved: false,
-		FullText:   fullText,
-		Channel:    "telegram",
+		ID:      "",
+		RawText: message.Text,
+		Channel: "telegram",
+		Epoch:   int64(message.Date),
 	}
 }
 
-func sendDataToBackend(text string) {
-	feedEntry := newFeedEntry(text)
+func sendDataToBackend(message *tgbotapi.Message) bool {
+	// Checking for api key
+	apiKey := os.Getenv("BACKEND_GO_API_KEY")
+	if apiKey == "" {
+		panic("BACKEND_GO_API_KEY is not found")
+	}
+
+	// Generating feed entry
+	feedEntry := newFeedEntry(message)
 	serialized, err := json.Marshal(feedEntry)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error occurred while serializing feed entry: %s \n", err.Error())
-		return
+		return false
 	}
 
+	// creating request
 	req, err := http.NewRequest(http.MethodPost, backendURL, bytes.NewReader(serialized))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not prepare request data to backend: %s", err.Error())
-		return
+		return false
 	}
 
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "AfetHarita "+os.Getenv("BACKEND_TOKEN"))
+	req.Header.Add("X-Api-Key", apiKey)
 
+	// sending request
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not send data to backend: %s", err.Error())
-		return
+		return false
 	}
 
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 { // 2xx
+	if resp.StatusCode == 201 {
 		fmt.Fprintf(os.Stderr, "an error on backend response: %s", err.Error())
-		return
+		return false
 	}
+	return true
 }
 
 func main() {
@@ -79,9 +93,8 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil {
-			// TODO send username: username, time_sent: timestamp in the future as extraParams
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-			sendDataToBackend(update.Message.Text)
+			go sendDataToBackend(update.Message)
 		}
 	}
 }
